d669dad9ab247c997bbbe23d89e98ff9550fe0b4
Nathan <nat.rougier38@gmail.com>
2021-10-05 18:41:50 -0400

    Accept dans terminal

--- ServerApplication/src/terminal.h
+++ ServerApplication/src/terminal.h
@@ -2,13 +2,19 @@
 // Created by nicol on 01/10/2021.
 //
 
+#include <WinSock2.h>
+#include <winsock.h>
+#include <stdio.h>
+#include "network.h"
 #ifndef RES_TP1_TERMINAL_H
 #define RES_TP1_TERMINAL_H
 
 
-class terminal {
-private:
-public:
+class terminal : public network {
+private: 
+	SOCKET clientSocket = INVALID_SOCKET;
+public: 
+	terminal();
 };
 
 
